f65b246b419989f21c591c4a407afa89537bf0d8
alejandro <amoro@alumni.unav.es>
2021-07-27 15:24:55 +0200

    refactored some code

--- Activations/__init__.py
+++ Activations/__init__.py
@@ -35,19 +35,21 @@ class Activation_Linear():
 
 class Activation_Sigmoid():
 
-  def forward(self, inputs):
+  def normalized_sigmoid(inputs):
     # this could potentially cause an overflow because of the exp explosion
     # normally this is the formula for the sigmoid 1 / (1 + np.exp(-inputs))
     # for an exp big enough we could run into an overflow
-    self.inputs = inputs
-
     # this is a trick to apply a function element-wise for a np array
     # this is a normalized sigmoid to prevent problems derive from huge exponentials
     # https://timvieira.github.io/blog/post/2014/02/11/exp-normalize-trick/
-    self.output = np.vectorize(
+    return np.vectorize(
       lambda x: 1 / (1 + exp(-x)) if x<0 else exp(x) / (1 + exp(x))
     )(inputs)
 
+  def forward(self, inputs):
+    self.inputs = inputs
+    self.output = self.normalized_sigmoid(inputs)
+
   def backward(self, dvalues):
     self.dvalues = dvalues
-    self.dinputs = 1 / (1 + np.exp(-dvalues)) * (1 - 1 / (1 + np.exp(-dvalues)))
+    self.dinputs = self.normalized_sigmoid(dvalues) * (1 - self.normalized_sigmoid(dvalues))
