7799027c0b5587c04ac240912e728074f9fb98b7
alejandro <amoro@alumni.unav.es>
2021-08-06 23:55:31 +0200

    changed file name

---
+++ pytorch_practice/BRU.py
@@ -0,0 +1,77 @@
+import torch.nn as nn
+import torch
+
+
+def eru(input: torch.Tensor, *, radix:float=1.) -> torch.Tensor:
+  i_radix = 1/radix
+  s_radix = radix ** radix
+
+  negative_case_tensor = torch.exp(torch.mul(input, radix)) - i_radix
+  positive_or_zero_case_tensor = torch.pow((torch.mul(input, s_radix) + 1), i_radix) - i_radix
+
+  return torch.where(input < 0,
+                    negative_case_tensor,
+                    positive_or_zero_case_tensor)
+
+
+def oru(input: torch.Tensor, *, radix:float=1.) -> torch.Tensor:
+  i_radix = 1/radix
+  s_radix = radix ** radix
+
+  return torch.mul(
+    torch.sign(input),
+    torch.pow((torch.mul(torch.abs(input), s_radix) + 1), i_radix) - 1)
+
+
+class ERU(nn.Module):
+  '''
+  Applies the Exponential Root Unit Unit (ERU) function element-wise.
+  Args:
+    radix: is a positive shape parameter that defines the choice of non-linearity
+  Shape:
+    - Input: (N, *) where * means, any number of additional
+      dimensions
+    - Output: (N, *), same shape as the input
+
+  References:
+    -  Related paper:
+    https://arxiv.org/pdf/1804.11237.pdf
+
+  Examples:
+    >>> e1ru = ERU(radix=1)
+    >>> input = torch.randn(2)
+    >>> output = e1ru(input)
+  '''
+  def __init__(self, radix:float=1.):
+    super().__init__()
+    self.radix = radix
+
+  def forward(self, input: torch.Tensor) -> torch.Tensor:
+    return eru(input, radix=self.radix)
+
+
+class ORU(nn.Module):
+  '''
+  Applies the Odd Root Unit Unit (ORU) function element-wise.
+  Args:
+    radix: is a positive shape parameter that defines the choice of non-linearity
+  Shape:
+    - Input: (N, *) where * means, any number of additional
+      dimensions
+    - Output: (N, *), same shape as the input
+
+  References:
+    -  Related paper:
+    https://arxiv.org/pdf/1804.11237.pdf
+
+  Examples:
+    >>> o1ru = ORU(radix=1)
+    >>> input = torch.randn(2)
+    >>> output = o1ru(input)
+  '''
+  def __init__(self, radix:float=1.):
+    super().__init__()
+    self.radix = radix
+
+  def forward(self, input: torch.Tensor) -> torch.Tensor:
+    return oru(input, radix=self.radix)
