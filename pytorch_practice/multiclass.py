55c5afc618a6e78832fbe21dee889280d27c8de8
alejandro <amoro@alumni.unav.es>
2021-08-13 10:15:19 +0200

    Added some string format back

--- pytorch_practice/multiclass.py
+++ pytorch_practice/multiclass.py
@@ -151,7 +151,7 @@ def plot_confusion_matrix(cm, classes,
   fmt = '.2f' if normalize else 'd'
   thresh = cm.max() / 2.
   for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-      plt.text(j, i, cm[i, j],
+      plt.text(j, i, cm[i, j], fmt,
                 horizontalalignment="center",
                 color="white" if cm[i, j] > thresh else "black")
 
