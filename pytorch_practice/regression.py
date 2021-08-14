34f7e7a0623428bfa68af6fa279100f3ec164bb7
alejandro <amoro@alumni.unav.es>
2021-08-14 10:31:35 +0200

    More stuff added to regression

--- pytorch_practice/regression.py
+++ pytorch_practice/regression.py
@@ -20,5 +20,26 @@ model = nn.Sequential(
     nn.Linear(128, 1)
 )
 
-loss = nn.MSELoss()
+loss_function = nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters())
+
+n_epochs = 3000
+
+recorded_losses = np.zeros(3000)
+
+for i in range(n_epochs):
+  epoch = i + 1
+  optimizer.zero_grad()
+
+  output = model(torch.from_numpy(X.astype(np.float32)))
+  loss = loss_function(output, torch.from_numpy(Y.astype(np.float32)).reshape(-1, 1))
+
+  loss.backward()
+  optimizer.step()
+
+  recorded_losses[i] = loss.item()
+
+  if epoch % 500:
+    print(f'Epoch: {i+1}, Loss: {loss.item()}')
+
+plt.plot(recorded_losses)
