bbd4d88ccdcd981b42a4bb80e4e3a5dcc6a7aef9
alejandro <amoro@alumni.unav.es>
2021-08-15 17:51:45 +0200

    CNN in pytorch

--- pytorch_practice/multiclass_cnn.py
+++ pytorch_practice/multiclass_cnn.py
@@ -12,6 +12,7 @@ import torchvision.transforms as transforms
 import cv2
 from sklearn.metrics import confusion_matrix
 import itertools
+import datetime
 
 train_dataset = torchvision.datasets.FashionMNIST(
     root='./datasets',
@@ -27,3 +28,92 @@ test_dataset = torchvision.datasets.FashionMNIST(
     download=True
 )
 
+print('Train Dataset shape', train_dataset.data.shape)
+print('Test Dataset shape', test_dataset.data.shape)
+
+K = len(set(train_dataset.targets.numpy()))
+print(f'Number of classes: {K}')
+
+class CNN(nn.Module):
+  def __init__(self, K):
+    super(CNN, self).__init__()
+    self.conv_layers = nn.Sequential(
+      nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, stride=2),
+      nn.ReLU(),
+      nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=2),
+      nn.ReLU(),
+      nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=2),
+      nn.ReLU()
+    )
+    self.dense_layers = nn.Sequential(
+      nn.Dropout(0.2),
+      nn.Linear(128 * 2 * 2, 512),
+      nn.ReLU()
+    )
+
+  def forward(self, X):
+    output = self.conv_layers(X)
+    output.view(output.size(0), -1)
+    return self.dense_layers(output)
+
+
+model = CNN(K)
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(device)
+model.to(device)
+
+loss_function = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters())
+
+batch_size = 128
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                           batch_size=batch_size,
+                                           shuffle=True)
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                           batch_size=batch_size,
+                                           shuffle=False)
+
+def train(model, criterion, optimizer, train_loader, test_loader, epochs):
+  train_losses = np.zeros(epochs)
+  test_losses = np.zeros(epochs)
+
+  for i in range(epochs):
+    t0 = datetime.now()
+    train_loss = []
+
+    for inputs, targets in train_loader:
+      inputs, targets = inputs.to(device), targets.to(device)
+
+      optimizer.zero_grad()
+
+      outputs = model(inputs)
+      loss = criterion(outputs, targets)
+
+      loss.backward()
+      optimizer.step()
+
+      train_loss.append(loss.item())
+    
+    train_loss = np.mean(train_loss)
+
+    test_loss = []
+    for inputs, targets in test_loader:
+      inputs, targets = inputs.to(device), targets.to(device)
+      outputs = model(inputs)
+      loss = criterion(outputs, targets)
+      test_loss.append(loss.item())
+
+    test_loss = np.mean(test_loss)
+
+    train_losses[i] = train_loss
+    test_losses[i] = test_loss
+
+    dt = datetime.now() - t0
+    print(f'Epoch {i+1}/{epochs}, Train Loss: {train_loss:.4f}, \
+          Test Loss: {test_loss:.4f}, Duration: {dt}')
+
+  return train_losses, test_losses
+
+train_losses, test_losses = train(
+  model, loss_function, optimizer, train_loader, test_loader, epochs=15)
