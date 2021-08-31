352da79b5d1b7276dbed6b65bab83ea38b1cd3f3
alejandro <amoro@alumni.unav.es>
2021-08-31 10:30:15 +0100

    created file to hold GAN related practice

---
+++ pytorch_practice/basic_gan.py
@@ -0,0 +1,8 @@
+import torch, pdb
+from torch.utils.data import DataLoader
+from torch import nn
+from torchvision import transforms
+from torchvision.datasets import MNIST
+from torchvision.utils import make_grid
+from tqdm.auto import tqdm
+import matplotlib.pyplot as plt
