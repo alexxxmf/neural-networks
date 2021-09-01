f46d84bcbea31e0ea6767ea85f5b58b9a8ae87c9
alejandro <amoro@alumni.unav.es>
2021-09-01 10:31:45 +0100

    added function to show images in grid for comparisons purposes

--- pytorch_practice/basic_gan.py
+++ pytorch_practice/basic_gan.py
@@ -6,3 +6,10 @@ from torchvision.datasets import MNIST
 from torchvision.utils import make_grid
 from tqdm.auto import tqdm
 import matplotlib.pyplot as plt
+
+
+def show(tensor, channels=1, size=(28,28), num=16):
+  data = tensor.detach().cpu().view(-1, channels, *size)
+  grid = make_grid(data[:num], n_rows=4).permute(1,2,0)
+  plt.imshow(grid)
+  plt.show()
