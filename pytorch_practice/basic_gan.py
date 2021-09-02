832cb55807f0b172aae4854046ff9db7a55f80d9
alejandro <amoro@alumni.unav.es>
2021-09-02 10:41:48 +0100

    added some config variables/hyper parameters

--- pytorch_practice/basic_gan.py
+++ pytorch_practice/basic_gan.py
@@ -13,3 +13,20 @@ def show(tensor, channels=1, size=(28,28), num=16):
   grid = make_grid(data[:num], n_rows=4).permute(1,2,0)
   plt.imshow(grid)
   plt.show()
+
+epochs = 300
+current_step = 0
+print_every = 100
+mean_generator_loss = 0
+mean_discriminator_loss = 0
+
+z_dim = 64
+learning_rate = 0.00001
+loss_func = nn.BCEWithLogitsLoss()
+
+batch_size = 128
+device = 'cuda'
+
+dataloader = DataLoader(MNIST('.', download=True, transform=transforms.ToTensor()), shuffle=True, batch_size=batch_size)
+
+# no steps 60000 images / 128 ~ 468
